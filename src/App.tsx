8d10a62aa53e2d9849531aeef60bd39ce23e18d8
Mitchell Robinson <mitchell@joinyaa.com>
2024-01-10 14:38:01 -0600

    CRA with TypeScript ReactGrid Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  CellChange,
+  Column,
+  DefaultCellTypes,
+  ReactGrid,
+  Row,
+} from "@silevis/reactgrid";
+import "@silevis/reactgrid/styles.css";
+import * as React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Person {
+  name: string;
+  surname: string;
 }
 
+const getPeople = (): Person[] => [
+  { name: "Thomas", surname: "Goldman" },
+  { name: "Susie", surname: "Quattro" },
+  { name: "", surname: "" },
+];
+
+const getColumns = (): Column[] => [
+  { columnId: "name", width: 150 },
+  { columnId: "surname", width: 150 },
+];
+
+const headerRow: Row = {
+  rowId: "header",
+  cells: [
+    { type: "header", text: "Name" },
+    { type: "header", text: "Surname" },
+  ],
+};
+
+const getRows = (people: Person[]): Row[] => [
+  headerRow,
+  ...people.map<Row>((person, idx) => ({
+    rowId: idx,
+    cells: [
+      { type: "text", text: person.name },
+      { type: "text", text: person.surname },
+    ],
+  })),
+];
+
+const handleChanges = (changes: CellChange<DefaultCellTypes>[]) => {
+  console.log(changes);
+};
+
+function App() {
+  const [people] = React.useState<Person[]>(getPeople());
+
+  const rows = getRows(people);
+  const columns = getColumns();
+
+  return (
+    <>
+      <ReactGrid rows={rows} columns={columns} onCellsChanged={handleChanges} />
+    </>
+  );
+}
 export default App;
